2288558cc869bbbb22bfb0d3c5b54cf3d272d52f
Keke <24128079@qq.com>
2017-07-20 17:42:16 +0800

    20170720

--- Code/YuYue/src/test/java/com/yuyue/test/dao/UserMappperTest.java
+++ Code/YuYue/src/test/java/com/yuyue/test/dao/UserMappperTest.java
@@ -1,27 +1,26 @@
 package com.yuyue.test.dao;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yuyue.mapper.UserMapper;
-import com.yuyue.model.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
 public class UserMappperTest {
+	private static final Logger LOGGER = Logger
+			.getLogger(UserMappperTest.class);
 	@Autowired
 	UserMapper userMapper;
 
 	@Test
 	public void testSelectByWxId() {
-		User user = userMapper.selectByWxId("111");
-		if (user == null) {
-			System.out.println("user null");
-		} else {
-			System.out.println(user.toString());
-		}
+	LOGGER.debug(JSONObject.toJSON(userMapper.selectByWxId("wxid1")));
 	}
 }
