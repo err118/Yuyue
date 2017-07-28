05a845d0cb7a75bfb22a119ba3654d5ffc76b414
Keke <24128079@qq.com>
2017-07-28 18:00:05 +0800

    增加首页server字段

---
+++ Code/YuYue/src/main/java/com/yuyue/model/Server.java
@@ -0,0 +1,11 @@
+package com.yuyue.model;
+
+public class Server {
+	private Long userId;
+	private String imageUrl;
+	private String nickname;
+	private String shopName;
+	public Long getUserId() {
+		return userId;
+	}
+}
